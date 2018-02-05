0be64072911df7e467eb01552b5d23d8a8f04933
Anton Sivov <anton.sivov@inbox.ru>
2018-02-05 21:38:59 +0300

    Rebase to test

---
+++ src/Lykke.Service.PayInternal/Models/TransferSourcesRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Lykke.Service.PayInternal.Core.Domain.Transfer;
+
+namespace Lykke.Service.PayInternal.Models
+{
+    public class TransferSourcesRequestModel : TransferRequestModel, ISourcesTransferRequest
+    {
+        public IEnumerable<ISourceAmount> SourceAddresses { get; set; }
+    }
+}
