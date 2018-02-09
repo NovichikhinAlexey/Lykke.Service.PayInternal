e33d18c2e34f143b3199c9210f2afc18de5392d2
tarurar <tarutin_andrew@mail.ru>
2018-02-09 17:16:58 +0200

    added validation for orders and transactions controllers

---
+++ src/Lykke.Service.PayInternal.Core/Exceptions/TransactionNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lykke.Service.PayInternal.Core.Exceptions
+{
+    public class TransactionNotFoundException : Exception
+    {
+        public TransactionNotFoundException()
+        {
+        }
+
+        public TransactionNotFoundException(string transactionId) : base("Transaction not found")
+        {
+            TransactionId = transactionId;
+        }
+
+        public TransactionNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TransactionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public string TransactionId { get; set; }
+    }
+}
--- src/Lykke.Service.PayInternal/Models/UpdateTransactionRequest.cs
+++ src/Lykke.Service.PayInternal/Models/UpdateTransactionRequest.cs
@@ -1,13 +1,23 @@
-﻿using Lykke.Service.PayInternal.Core.Domain.Transaction;
+﻿using System.ComponentModel.DataAnnotations;
+using Lykke.Service.PayInternal.Core.Domain.Transaction;
 
 namespace Lykke.Service.PayInternal.Models
 {
     public class UpdateTransactionRequest : IUpdateTransactionRequest
     {
+        [Required]
         public string TransactionId { get; set; }
+
+        [Required]
         public string WalletAddress { get; set; }
+
+        [Required]
         public double Amount { get; set; }
+
+        [Required]
         public int Confirmations { get; set; }
+
+        [Required]
         public string BlockId { get; set; }
     }
 }
