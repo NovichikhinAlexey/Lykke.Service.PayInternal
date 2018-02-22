588a1149a4e12c90e79bec7b4030cfe3b363c6ec
tarurar <tarutin_andrew@mail.ru>
2018-02-22 12:10:58 +0300

    updated client with assets controller

---
+++ client/Lykke.Service.PayInternal.Client/Models/Asset/AssetAvailabilityType.cs
@@ -0,0 +1,18 @@
+﻿namespace Lykke.Service.PayInternal.Client.Models.Asset
+{
+    /// <summary>
+    /// Asset availability type
+    /// </summary>
+    public enum AssetAvailabilityType
+    {
+        /// <summary>
+        /// Asset is available to be used as payment asset
+        /// </summary>
+        Payment,
+
+        /// <summary>
+        /// Asset is available to be used as settlement asset
+        /// </summary>
+        Settlement
+    }
+}
