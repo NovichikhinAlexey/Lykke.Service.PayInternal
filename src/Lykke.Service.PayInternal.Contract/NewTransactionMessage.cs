f26fde2072ba0c61ec03682d682da01c0e98daa8
tarurar <tarutin_andrew@mail.ru>
2018-03-09 22:58:37 +0300

    updated getting monitored transactions controller

--- src/Lykke.Service.PayInternal.Contract/NewTransactionMessage.cs
+++ src/Lykke.Service.PayInternal.Contract/NewTransactionMessage.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lykke.Service.PayInternal.Contract
 {
-    public class TransactionStateResponse
+    public class NewTransactionMessage
     {
         public string Id { get; set; }
         public double Amount { get; set; }
@@ -8,6 +8,5 @@
         public string BlockId { get; set; }
         public string Blockchain { get; set; }
         public int Confirmations { get; set; }
-        // Without WalletAddress - more common case.
     }
 }
