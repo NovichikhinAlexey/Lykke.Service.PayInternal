9e55941b145986174d686f5bff1a3aab73905c75
tarurar <tarutin_andrew@mail.ru>
2018-03-29 13:47:50 +0300

    added string consts to be used in public APIs

---
+++ src/Lykke.Service.PayInternal.Contract/PaymentRequest/PaymentRequestPublicStatuses.cs
@@ -0,0 +1,39 @@
+﻿namespace Lykke.Service.PayInternal.Contract.PaymentRequest
+{
+    /// <summary>
+    /// Payment request statuses being used for public APIs
+    /// </summary>
+    public static class PaymentRequestPublicStatuses
+    {
+        /// <summary>
+        /// Payment request has been created
+        /// </summary>
+        public const string PaymentRequestCreated = "PAYMENT_REQUEST_CREATED";
+
+        /// <summary>
+        /// Payment has been confirmed
+        /// </summary>
+        public const string PaymentConfirmed = "PAYMENT_CONFIRMED";
+
+        /// <summary>
+        /// Payment is in progress
+        /// </summary>
+        public const string PaymentInProgress = "PAYMENT_INPROGRESS";
+
+        /// <summary>
+        /// Payment error
+        /// </summary>
+        public const string PaymentError = "PAYMENT_ERROR";
+
+
+        /// <summary>
+        /// Refund is in progress
+        /// </summary>
+        public const string RefundInProgress = "REFUND_INPROGRESS";
+
+        /// <summary>
+        /// Refund has been confirmed
+        /// </summary>
+        public const string RefundConfirmed = "REFUND_CONFIRMED";
+    }
+}
