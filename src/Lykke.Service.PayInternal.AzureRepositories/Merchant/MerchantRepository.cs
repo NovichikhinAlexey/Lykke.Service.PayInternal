22c0ee67b62ea947e06640c3368f1a5a3f2c2ef2
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-04-09 02:18:19 +0300

    LPDEV-158 Add merchants with same api key is not allowed

--- src/Lykke.Service.PayInternal.AzureRepositories/Merchant/MerchantRepository.cs
+++ src/Lykke.Service.PayInternal.AzureRepositories/Merchant/MerchantRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AzureStorage;
@@ -31,6 +30,13 @@ namespace Lykke.Service.PayInternal.AzureRepositories.Merchant
         public async Task<IMerchant> GetAsync(string merchantName)
         {
             return await _storage.GetDataAsync(GetPartitionKey(), GetRowKey(merchantName));
+        }
+
+        public async Task<IReadOnlyList<IMerchant>> FindAsync(string apiKey)
+        {
+            IList<MerchantEntity> entities = await _storage.GetDataAsync(merchant => merchant.ApiKey == apiKey);
+
+            return entities.ToList();
         }
 
         public async Task<IMerchant> InsertAsync(IMerchant merchant)
