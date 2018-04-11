d6e00667b2aba89b4977fa4881db76a3ca7e0234
tarurar <tarutin_andrew@mail.ru>
2018-04-11 12:46:57 +0300

    added test to chec controllers initialization (resolving all dependencies to check if all of them were registered)

--- tests/Lykke.Service.PayInternal.Tests/ControllerInitTests.cs
+++ tests/Lykke.Service.PayInternal.Tests/ControllerInitTests.cs
@@ -1,58 +1,106 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.IO;
+using Autofac;
+using Common.Log;
+using Lykke.Service.PayInternal.Core.Settings;
+using Lykke.Service.PayInternal.Modules;
+using Lykke.SettingsReader;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Lykke.Service.PayInternal.Tests
 {
     [TestClass]
     public class ControllerInitTests
     {
-        [TestMethod]
-        public void AllApiControllersINitializationTest()
+        [TestInitialize]
+        public void Initialize()
         {
-            //var configBuilder = new ConfigurationBuilder();
+            using (var file = File.OpenText(@"Properties\launchSettings.json"))
+            {
+                var reader = new JsonTextReader(file);
 
-            //configBuilder.AddEnvironmentVariables();
+                var jObject = JObject.Load(reader);
 
-            //var configuration = configBuilder.Build();
+                var variables = jObject
+                    .GetValue("profiles")
+                    .SelectMany(profiles => profiles.Children())
+                    .SelectMany(profile => profile.Children<JProperty>())
+                    .Where(prop => prop.Name == "environmentVariables")
+                    .SelectMany(prop => prop.Value.Children<JProperty>())
+                    .ToList();
 
-            //configuration["SettingsUrl"] = configuration["SettingsUrlForApiTests"];
+                foreach (var variable in variables)
+                {
+                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                }
+            }
+        }
 
-            //var binder = new AzureBinder();
-            //var builder = binder.Bind(configuration, null, true);
-            //builder.RegisterInstance(new Mock<IHostingEnvironment>().Object).As<IHostingEnvironment>();
-            //builder.RegisterInstance(new Mock<IHttpContextAccessor>().Object).As<IHttpContextAccessor>();
-            //var controllerTypes = typeof(Startup).Assembly
-            //    .GetExportedTypes()
-            //    .Where(t => t.IsSubclassOf(typeof(Controller)))
-            //    .ToList();
+        [TestMethod]
+        public void AllApiControllersInitializationTest()
+        {
+            var configBuilder = new ConfigurationBuilder();
 
-            //foreach (var type in controllerTypes)
-            //    builder.RegisterType(type);
+            configBuilder.AddEnvironmentVariables();
 
-            //var ioc = builder.Build();
+            var configuration = configBuilder.Build();
 
-            //var failedControllers = new List<string>();
-            //foreach (var type in controllerTypes)
-            //{
-            //    try
-            //    {
-            //        ioc.Resolve(type);
-            //    }
-            //    catch (Exception)
-            //    {
-            //        failedControllers.Add(type.Name);
-            //    }
-            //}
-            //if (failedControllers.Count > 0)
-            //    throw new Exception($"These WalletApi controllers can't be instantiated: {string.Join(",", failedControllers)}");
-            //Assert.IsTrue(true);
+            configuration["SettingsUrl"] = configuration["SettingsUrlForApiTests"];
+
+            var builder = new ContainerBuilder();
+
+            var appSettings = configuration.LoadSettings<AppSettings>();
+
+            builder.RegisterModule(new AzureRepositories.AutofacModule(
+                appSettings.Nested(o => o.PayInternalService.Db.MerchantOrderConnString),
+                appSettings.Nested(o => o.PayInternalService.Db.MerchantConnString),
+                appSettings.Nested(o => o.PayInternalService.Db.PaymentRequestConnString),
+                appSettings.Nested(o => o.PayInternalService.Db.TransferConnString),
+                new Mock<ILog>().Object));
+
+            builder.RegisterModule(new Services.AutofacModule(
+                appSettings.CurrentValue.PayInternalService.ExpirationPeriods.Order,
+                appSettings.CurrentValue.PayInternalService.ExpirationPeriods.Refund,
+                appSettings.CurrentValue.PayInternalService.TransactionConfirmationCount,
+                appSettings.CurrentValue.PayInternalService.Blockchain.WalletAllocationPolicy.Policies));
+
+            builder.RegisterModule(new ServiceModule(appSettings, new Mock<ILog>().Object));
+
+            var controllerTypes = typeof(Startup).Assembly
+                .GetExportedTypes()
+                .Where(t => t.IsSubclassOf(typeof(Controller)))
+                .ToList();
+
+            foreach (var type in controllerTypes)
+                builder.RegisterType(type);
+
+            var ioc = builder.Build();
+
+            var failedControllers = new List<string>();
+
+            foreach (var type in controllerTypes)
+            {
+                try
+                {
+                    ioc.Resolve(type);
+                }
+                catch (Exception)
+                {
+                    failedControllers.Add(type.Name);
+                }
+            }
+
+            if (failedControllers.Count > 0)
+                throw new Exception($"These API controllers can't be instantiated: {string.Join(",", failedControllers)}");
+
+            Assert.IsTrue(true);
         }
     }
 }
