84b05b7d23d510f901b1ef32e412a679aec08400
tarurar <tarutin_andrew@mail.ru>
2018-04-28 23:18:06 +0300

    added WalletNotFoundException details logging

--- src/Lykke.Service.PayInternal/Mapping/PaymentTxBcnWalletAddressValueResolver.cs
+++ src/Lykke.Service.PayInternal/Mapping/PaymentTxBcnWalletAddressValueResolver.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using Common;
+using Common.Log;
 using Lykke.Service.PayInternal.Core.Domain.Transaction;
+using Lykke.Service.PayInternal.Core.Exceptions;
 using Lykke.Service.PayInternal.Core.Services;
 
 namespace Lykke.Service.PayInternal.Mapping
@@ -7,17 +10,29 @@ namespace Lykke.Service.PayInternal.Mapping
     public class PaymentTxBcnWalletAddressValueResolver : IValueResolver<IPaymentRequestTransaction, object, string>
     {
         private readonly IWalletManager _walletManager;
+        private readonly ILog _log;
 
-        public PaymentTxBcnWalletAddressValueResolver(IWalletManager walletManager)
+        public PaymentTxBcnWalletAddressValueResolver(IWalletManager walletManager, ILog log)
         {
             _walletManager = walletManager;
+            _log = log;
         }
 
         public string Resolve(IPaymentRequestTransaction source, object destination, string destMember,
             ResolutionContext context)
         {
-            return _walletManager.ResolveBlockchainAddressAsync(source.WalletAddress, source.AssetId).GetAwaiter()
-                .GetResult();
+            try
+            {
+                return _walletManager.ResolveBlockchainAddressAsync(source.WalletAddress, source.AssetId).GetAwaiter()
+                    .GetResult();
+            }
+            catch (WalletNotFoundException ex)
+            {
+                _log.WriteErrorAsync(nameof(PaymentTxBcnWalletAddressValueResolver), nameof(Resolve),
+                    new { ex.WalletAddress }.ToJson(), ex);
+
+                throw;
+            }
         }
     }
 }
