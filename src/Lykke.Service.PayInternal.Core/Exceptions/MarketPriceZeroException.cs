be834e6aa62e67b76d2e0c6ff263539d1d327543
tarurar <tarutin_andrew@mail.ru>
2018-05-16 10:36:00 +0300

    fixed code review comments

---
+++ src/Lykke.Service.PayInternal.Core/Exceptions/MarketPriceZeroException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lykke.Service.PayInternal.Core.Exceptions
+{
+    public class MarketPriceZeroException : Exception
+    {
+        public MarketPriceZeroException()
+        {
+        }
+
+        public MarketPriceZeroException(string priceType) : base("Market price is 0")
+        {
+            PriceType = priceType;
+        }
+
+        public MarketPriceZeroException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MarketPriceZeroException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public string PriceType { get; set; }
+    }
+}
