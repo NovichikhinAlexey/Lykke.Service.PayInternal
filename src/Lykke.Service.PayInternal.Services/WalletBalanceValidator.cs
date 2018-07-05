fdf629ae9dec9f585a594963d8f415f5795299aa
tarurar <tarutin_andrew@mail.ru>
2018-07-05 11:39:55 +0300

    fixed issue with asseId vs assetDisplayId when getting balances from blockchain

--- src/Lykke.Service.PayInternal.Services/WalletBalanceValidator.cs
+++ src/Lykke.Service.PayInternal.Services/WalletBalanceValidator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Common;
 using JetBrains.Annotations;
 using Lykke.Service.PayInternal.Core;
 using Lykke.Service.PayInternal.Core.Exceptions;
@@ -11,17 +12,22 @@ namespace Lykke.Service.PayInternal.Services
     {
         private readonly IAssetSettingsService _assetSettingsService;
         private readonly IBlockchainClientProvider _blockchainClientProvider;
+        private readonly ILykkeAssetsResolver _lykkeAssetsResolver;
 
         public WalletBalanceValidator(
             [NotNull] IAssetSettingsService assetSettingsService, 
-            [NotNull] IBlockchainClientProvider blockchainClientProvider)
+            [NotNull] IBlockchainClientProvider blockchainClientProvider, 
+            [NotNull] ILykkeAssetsResolver lykkeAssetsResolver)
         {
             _assetSettingsService = assetSettingsService ?? throw new ArgumentNullException(nameof(assetSettingsService));
             _blockchainClientProvider = blockchainClientProvider ?? throw new ArgumentNullException(nameof(blockchainClientProvider));
+            _lykkeAssetsResolver = lykkeAssetsResolver ?? throw new ArgumentNullException(nameof(lykkeAssetsResolver));
         }
 
         public async Task ValidateTransfer(string walletAddress, string assetId, decimal transferAmount)
         {
+            assetId = assetId.IsGuid() ? assetId : await _lykkeAssetsResolver.GetLykkeId(assetId);
+
             BlockchainType network = await _assetSettingsService.GetNetworkAsync(assetId);
 
             IBlockchainApiClient blockchainApiClient = _blockchainClientProvider.Get(network);
