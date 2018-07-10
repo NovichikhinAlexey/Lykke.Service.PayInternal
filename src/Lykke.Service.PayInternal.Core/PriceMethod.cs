d7658b13c98f8c4452f2cd08e3143f464cd8a1bb
tarurar <tarutin_andrew@mail.ru>
2018-07-10 11:54:05 +0300

    fixed 500 on sum calculation

--- src/Lykke.Service.PayInternal.Core/PriceMethod.cs
+++ src/Lykke.Service.PayInternal.Core/PriceMethod.cs
@@ -1,5 +1,9 @@
-﻿namespace Lykke.Service.PayInternal.Core
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Lykke.Service.PayInternal.Core
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum PriceMethod
     {
         None = 0,
