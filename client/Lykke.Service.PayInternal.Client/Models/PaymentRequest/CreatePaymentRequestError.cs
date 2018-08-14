a775cc8be23fb0615c41d46ebb73a32413758866
tarurar <tarutin_andrew@mail.ru>
2018-08-14 19:50:50 +0300

    added error codes when creating payment request

---
+++ client/Lykke.Service.PayInternal.Client/Models/PaymentRequest/CreatePaymentRequestError.cs
@@ -0,0 +1,7 @@
+﻿namespace Lykke.Service.PayInternal.Client.Models.PaymentRequest
+{
+    public class CreatePaymentRequestError
+    {
+        public CreatePaymentRequestErrorType Code { get; set; }
+    }
+}
