e39444bfe5ab6488993faab33fb22bbccb923f4f
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-09-13 13:51:15 +0300

    Add settlement statuses to contracts LPDEV-807

--- src/Lykke.Service.PayInternal/Models/PaymentRequests/PaymentRequestDetailsModel.cs
+++ src/Lykke.Service.PayInternal/Models/PaymentRequests/PaymentRequestDetailsModel.cs
@@ -35,7 +35,7 @@ namespace Lykke.Service.PayInternal.Models.PaymentRequests
         
         public string WalletAddress { get; set; }
         
-        [EnumDataType(typeof(PaymentRequestStatus), ErrorMessage = "Invalid value, possible values are: None, New, Cancelled, InProcess, Confirmed, RefundInProgress, Refunded, Error")]
+        [EnumDataType(typeof(PaymentRequestStatus), ErrorMessage = "Invalid value, possible values are: None, New, Cancelled, InProcess, Confirmed, RefundInProgress, Refunded, Error, SettlementInProgress, SettlementError, Settled")]
         public PaymentRequestStatus Status { get; set; }
         
         public decimal PaidAmount { get; set; }
